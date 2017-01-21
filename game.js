4956d7fb29b7d722a016e744b16dae6bb9317acc
xaishinn <murraythemoose@gmail.com>
2017-01-21 15:12:36 -0700

    Updated Game Engine

--- game.js
+++ game.js
@@ -6,35 +6,40 @@ const game = new lib.Game('Escape');
 
 var hasKey = false;
 
-function c() {
+function cls() {
 	game.info("I'm in the closet");
 	return true;
 }
 
-function r() {
-	game.info("You found a key!");
+function key() {
+	game.success("You found a key!");
 	hasKey = true;
 	return true;
 }
 
-function s() {
+function sec() {
 	if (hasKey) {
 		game.info("Welcome to the secret room!");
-		return true;
+		game.finish('Congratulations! You have escaped Donald Trump!');
 	} else {
-		game.info("Go away!");
+		game.error("This room is locked.");
 		return false;
 	}
 }
 
 game.build([
-	[ '|', '+', '+', '+',  '+' ],
-	[ '|', 's', ' ', ' ',   c  ],
-	[ '|', ' ', '+',  r ,  '+' ],
-	[ '|', ' ', '+', '+',  '+' ],
-	[ '|', ' ', ' ', ' ',   s  ],
-	[ '|', '-', '|', '-',  '-' ] 
+	[ '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+'],
+	[ '+', 's', ' ', ' ', '+', '+', '+', '+', ' ', ' ', ' ', ' ', ' ', '+', '+'],
+	[ '+', ' ', '+', ' ', '+', '+', '+', '+', ' ', '+', '+', '+', ' ', '+', '+'],
+	[ '+', ' ', '+', ' ', '+', '+', '+', '+', ' ', '+', '+', '+', ' ', '+', '+'],
+	[ '+', ' ', '+', ' ', '+', '+', cls, '+', ' ', '+', '+', '+', ' ', '+', '+'],
+	[ '+', ' ', '+', ' ', '+', '+', ' ', '+', ' ', '+', ' ', ' ', ' ', ' ', '+'],
+	[ '+', ' ', '+', ' ', ' ', '+', ' ', ' ', ' ', '+', ' ', '+', '+', ' ', '+'],
+	[ '+', ' ', '+', '+', ' ', '+', ' ', '+', '+', '+', ' ', '+', '+', ' ', '+'],
+	[ '+', ' ', '+', '+', ' ', ' ', ' ', '+', sec, ' ', ' ', '+', '+', key, '+'],
+	[ '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+', '+'],
 ]);
+
 
 game.start();
 
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var DIRECTIONS = {
 	NORTH: 'north',
 	SOUTH: 'south',
 	EAST: 'east',
-	WEST: 'west'
+	WEST: 'west',
+	EXIT: 'exit'
 };
 
 var readlineSync = require('readline-sync');
@@ -29,24 +30,36 @@ function getDirection() {
 
 var userName = 'Username';
 
-userName = readlineSync.question('What is your name? ');
-console.log('Hi ' + userName + '! Welcome to my game!');
+userName = prompt('What is your name? ');
+console.log('welcome ' + userName + '! ... to my nightmare');
 
-var dir = getDirection();
+console.log("You are in a pitch black room and someone or something is following you.");
 
-if (dir === DIRECTIONS.NORTH) {
-	if (confirm('Are you sure you want to go north?')) {
-		console.log('You moved north!');
+console.log("You feel an intense need to escape but all you have is a dimly lit compass. Which direction do you want to go?")
+
+do{
+	var dir = getDirection();
+
+	if (dir === DIRECTIONS.NORTH) {
+		if (confirm('Are you sure you want to go north?')) {
+			console.log('You moved north. The Unknown is still following you.');
+		}
+		// G0 north
+	} else if (dir === DIRECTIONS.SOUTH) {
+		var reason = prompt('Why? ');
+
+		console.log('You moved south because: ' + reason);
+		// Go south
+	} else if (dir === DIRECTIONS.EAST) {
+		// Go east
+	} else if (dir === DIRECTIONS.WEST) {
+		// Go west
+	} else {
+		// I don't know where you're going
 	}
-	// G0 north
-} else if (dir === DIRECTIONS.SOUTH) {
-	var reason = prompt('Why? ');
 
-	console.log('You moved south because: ' + reason);
-	// Go south
-} else if (dir === DIRECTIONS.EAST) {
-	// Go east
-} else if (dir === DIRECTIONS.WEST) {
-	// Go west
-}
+
+}while(dir !== DIRECTIONS.EXIT);
+
+console.log('Goodbye');
 
