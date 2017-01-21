03d5b54b836cdbd8ac2ad1cb97ca0f73c3cd1bc8
Charlie McClung <charlie@cmr1.com>
2017-01-21 13:23:13 -0700

    Checkin

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 'use strict';
 
-const prompt = require('prompt');
+var DIRECTIONS = {
+	NORTH: 'north',
+	SOUTH: 'south',
+	EAST: 'east',
+	WEST: 'west'
+};
 
-console.log("You're in a dark room and something is following you.");
-console.log("There are exit signs to your left, right, and straight ahead. Where do you go?");
+var readlineSync = require('readline-sync');
 
-prompt.get('direction', (err, result) => {
-	if (err) {
-		console.log('ERROR!', err);
-	} else {
-		console.log(result);
+var select = readlineSync.keyInSelect;
+var prompt = readlineSync.question;
+var confirm = readlineSync.keyInYN;
+
+function getDirection() {
+	var dirs = Object.keys(DIRECTIONS).map(k => DIRECTIONS[k]);
+
+	var dirIndex = select(dirs, 'Which direction?');
+
+	var dir = DIRECTIONS[Object.keys(DIRECTIONS)[dirIndex]];
+
+	console.log('Moving ' + dir + ' ...');
+
+	return dir;
+}
+
+
+
+var userName = 'Username';
+
+userName = readlineSync.question('What is your name? ');
+console.log('Hi ' + userName + '! Welcome to my game!');
+
+var dir = getDirection();
+
+if (dir === DIRECTIONS.NORTH) {
+	if (confirm('Are you sure you want to go north?')) {
+		console.log('You moved north!');
 	}
-});
+	// G0 north
+} else if (dir === DIRECTIONS.SOUTH) {
+	var reason = prompt('Why? ');
+
+	console.log('You moved south because: ' + reason);
+	// Go south
+} else if (dir === DIRECTIONS.EAST) {
+	// Go east
+} else if (dir === DIRECTIONS.WEST) {
+	// Go west
+}
 
---
+++ test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const lib = require('./lib');
+
+const game = new lib.Game('Escape');
+
+var hasKey = false;
+
+function c() {
+	game.info("I'm in the closet");
+	return true;
+}
+
+function r() {
+	game.info("You found a key!");
+	hasKey = true;
+	return true;
+}
+
+function s() {
+	if (hasKey) {
+		game.info("Welcome to the secret room!");
+		return true;
+	} else {
+		game.info("Go away!");
+		return false;
+	}
+}
+
+game.build([
+	[ '|', '+', '+', '+',  '+' ],
+	[ '|', 's', ' ', ' ',   c  ],
+	[ '|', ' ', '+',  r ,  '+' ],
+	[ '|', ' ', '+', '+',  '+' ],
+	[ '|', ' ', ' ', ' ',   s  ],
+	[ '|', '-', '|', '-',  '-' ]
+]);
+
+game.start();
+
